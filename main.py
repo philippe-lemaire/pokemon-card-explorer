73ffbd9c33a3fecdc74e7e42ca1682fae1c28a15
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-05-12 21:52:10 +0200

    pulling a set seams to work but card dicts are not consistent.

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 import requests
 import os
 import utils
+import csv
 
 api_file = 'api-key/key'
 
 my_key = utils.get_file_contents(api_file)
+headers = {'X-Api-Key': my_key}
 
-base_uri = 'https://api.pokemontcg.io'
+base_uri = 'https://api.pokemontcg.io/v2/'
+
+def get_cards(query):
+    '''takes a query and returns a list of cards as dicts'''
+    params = {
+        'q': query,
+        'page': 1,
+        #'pageSize': pageSize,
+        ' orderBy': 'hp'
+    }
+    
+    endpoint = 'cards/'
+    url = base_uri + endpoint
+    response = requests.get(url, params=params, headers=headers).json()['data']
+    return response
+
+sets = ['set.id:sm1']
+
+for index, item in enumerate(sets):
+    cards = get_cards(item)
+
+for card in cards:
+    name = card['name']
+    supertype = card['supertype']
+    print(f"{name} is a {supertype} card")
+
+
+
 
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import csv
+
 def get_file_contents(filename):
     """ Given a filename,
         return the contents of that file
@@ -9,3 +11,13 @@ def get_file_contents(filename):
             return f.read().strip()
     except FileNotFoundError:
         print("'%s' file not found" % filename)
+
+
+def write_csv(series, cards):
+    '''takes a pokemon series such as 'sun and moon' and a list of dicts representing cards
+    and write a csv file for it in the cards folder'''
+    with open(f"cards/{series}.csv", 'w') as csv_file:
+        writer = csv.DictWriter(csv_file, cards[2].keys())
+        writer.writeheader()
+        for card in cards:
+            writer.writerow(card)
