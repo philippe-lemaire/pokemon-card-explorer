cfde95f55383bea47f100bcdb9d6aa62c176937b
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-05-15 20:51:52 +0200

    search and save all right

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import utils
 #sets = utils.get_sets('')
 #utils.write_to_csv(sets)
 
-dragons = utils.search_cards('supertype:pokemon types:dragon')
+#dragons = utils.search_cards('supertype:pokemon types:dragon')
+#utils.save_search_to_csv(dragons, 'dragons')
 
-utils.save_search_to_csv(dragons, 'dragons')
+#trainers = utils.search_cards('supertype:trainer set.id:sm10')
+#utils.save_search_to_csv(trainers, 'trainers_from_sm10')
+
+fire_pkm_from_last_set = utils.search_cards('supertype:pokemon types:fire set.id:swsh5')
+utils.save_search_to_csv(fire_pkm_from_last_set, 'fire_mons_from_battle_styles')
